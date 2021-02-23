72b230f306c6fa7f7bd05fe0030d8929920936b3
Pedro Afonso <pedroafonso23@yahoo.com.br>
2021-02-23 20:00:35 -0300

    commit 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+import { ExperienceBar } from "../components/ExperienceBar"
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="container">
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+      </Head>
+
+      <ExperienceBar />
+    </div>
   )
 }
